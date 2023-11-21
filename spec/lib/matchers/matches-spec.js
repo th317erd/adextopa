dd21ae7b77a5bc02230cf9f778298ee08411db2a
Wyatt Greenway <th317erd@gmail.com>
2023-11-20 20:16:14 -0700

    Matches now works with capture groups

--- spec/lib/matchers/matches-spec.js
+++ spec/lib/matchers/matches-spec.js
@@ -27,4 +27,19 @@ describe('/Core/Matchers/MatchesMatcher', () => {
     let result = await parser.exec(Matches(/test\s+\d+/i).name('TestToken'));
     expect(result).toMatchSnapshot();
   });
+
+  it('can properly use capture groups', async () => {
+    let result = await parser.exec(Matches(/test\s+(\d+)/i).name('TestToken'));
+    expect(result).toMatchSnapshot();
+  });
+
+  it('can properly use named capture groups', async () => {
+    let result = await parser.exec(Matches(/(?<word>\w+)\s+(?<value>\d+)/i).name('TestToken'));
+    expect(result).toMatchSnapshot();
+  });
+
+  it('can suppress array indices in captures', async () => {
+    let result = await parser.exec(Matches(/(?<word>\w+)\s+(?<value>\d+)/i).name('TestToken').suppressIndices(true));
+    expect(result).toMatchSnapshot();
+  });
 });
